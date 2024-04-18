7aad402adf6ec4aeb00379a6d99d397d7460a056
bobbycyl <bobbycyl@126.com>
2024-04-18 20:46:17 +0800

    modify style

--- osu_playlist_generator.py
+++ osu_playlist_generator.py
@@ -2,10 +2,11 @@ from clayutil.futil import OsuPlaylist
 
 oauth = "../osu.properties"
 playlist = "./playlists/2000pp过关.properties"
-o0 = OsuPlaylist(oauth)
+o0 = OsuPlaylist(oauth, True, False)
 font_difficulty = "../Kodchasan-Medium.ttf"
-font_title = "../AlibabaPuHuiTi-3-65-Medium.ttf"
+font_title = "../AlibabaPuHuiTi-3-75-SemiBold.ttf"
 font_artist = "../AlibabaPuHuiTi-3-55-Regular.ttf"
 font_mapped_by = "../Kodchasan-Italic.ttf"
-font_mapper = "../Kodchasan-Medium.ttf"
-o0.generate(playlist, font_difficulty, font_title, font_artist, font_mapped_by, font_mapper)
+font_mapper = "../Kodchasan-SemiBold.ttf"
+font_mono = "../HackNerdFontMono-Bold.ttf"
+o0.generate(playlist, font_difficulty, font_title, font_artist, font_mapped_by, font_mapper, font_mono)
