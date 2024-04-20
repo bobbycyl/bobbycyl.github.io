010df45655a1fa05457cc800d3d75f53b7fb3223
bobbycyl <bobbycyl@126.com>
2024-04-20 14:52:46 +0800

    use unifont as global font

--- osu_playlist_generator.py
+++ osu_playlist_generator.py
@@ -2,23 +2,18 @@ from clayutil.futil import OsuPlaylist
 
 oauth = "../osu.properties"
 normal_playlists = [
-    "./playlists/2000pp过关.properties"
+    "./playlists/2000pp过关.properties",
 ]
 match_playlists = [
-    "./playlists/HSC3S1R1.properties"
+    "./playlists/HSC3S1R1.properties",
 ]
 o0 = OsuPlaylist(oauth)
-font_difficulty = "../Kodchasan-Medium.ttf"
-font_title = "../AlibabaPuHuiTi-3-75-SemiBold.ttf"
-font_artist = "../AlibabaPuHuiTi-3-55-Regular.ttf"
-font_mapped_by = "../Kodchasan-Italic.ttf"
-font_mapper = "../Kodchasan-SemiBold.ttf"
-font_mono = "../HackNerdFontMono-Bold.ttf"
+font = "../unifont-15.1.05.otf"
 for playlist in normal_playlists:
     o0.draw_target = True
     o0.draw_difficulty_table = False
-    o0.generate(playlist, font_difficulty, font_title, font_artist, font_mapped_by, font_mapper, font_mono)
+    o0.generate(playlist, font)
 for playlist in match_playlists:
     o0.draw_target = False
     o0.draw_difficulty_table = True
-    o0.generate(playlist, font_difficulty, font_title, font_artist, font_mapped_by, font_mapper, font_mono)
+    o0.generate(playlist, font)
