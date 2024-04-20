ee3e0a71782209cc4cee5905d79a49fbee08f6c9
bobbycyl <bobbycyl@126.com>
2024-04-20 18:17:21 +0800

    revert filename rules with new logic and title

--- osu_playlist_generator.py
+++ osu_playlist_generator.py
@@ -1,26 +1,25 @@
 import os
 import re
+import shutil
 
 from clayutil.futil import OsuPlaylist
 
 
 oauth = "../osu.properties"
-original_playlist_pattern = re.compile(r"o\..*\.properties")
-match_playlist_pattern = re.compile(r"m\..*\.properties")
-normal_playlists = []
-match_playlists = []
-for i in os.listdir("./playlists"):
-    if original_playlist_pattern.match(i):
-        normal_playlists.append("./playlists/%s" % i)
-    if match_playlist_pattern.match(i):
-        match_playlists.append("./playlists/%s" % i)
+original_playlist_pattern = re.compile(r"O\.(.*\.properties)")
+match_playlist_pattern = re.compile(r"M\.(.*\.properties)")
 o0 = OsuPlaylist(oauth)
 font = "../unifont-15.1.05.otf"
-for playlist in normal_playlists:
-    o0.draw_target = True
-    o0.draw_difficulty_table = False
-    o0.generate(playlist, font)
-for playlist in match_playlists:
-    o0.draw_target = False
-    o0.draw_difficulty_table = True
-    o0.generate(playlist, font)
+for i in os.listdir("./playlists"):
+    if m := original_playlist_pattern.match(i):
+        shutil.copyfile("./playlists/%s" % m.group(0), "./playlists/%s" % m.group(1))
+        o0.draw_target = True
+        o0.draw_difficulty_table = False
+        o0.generate("./playlists/%s" % m.group(1), font, " — original playlist")
+        os.remove("./playlists/%s" % m.group(1))
+    if m := match_playlist_pattern.match(i):
+        shutil.copyfile("./playlists/%s" % m.group(0), "./playlists/%s" % m.group(1))
+        o0.draw_target = False
+        o0.draw_difficulty_table = True
+        o0.generate("./playlists/%s" % m.group(1), font, " — match playlist")
+        os.remove("./playlists/%s" % m.group(1))
