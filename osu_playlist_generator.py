510c57a151de6e989a964e2fd1d9345b3360bbe6
bobbycyl <bobbycyl@126.com>
2024-04-23 12:34:32 +0800

    feat: skip exists to make generation faster

--- osu_playlist_generator.py
+++ osu_playlist_generator.py
@@ -5,21 +5,27 @@ import shutil
 from clayutil.futil import OsuPlaylist
 
 
+fast_gen = True
 oauth = "../osu.properties"
-original_playlist_pattern = re.compile(r"O\.(.*\.properties)")
-match_playlist_pattern = re.compile(r"M\.(.*\.properties)")
+original_playlist_pattern = re.compile(r"O\.(.*)\.properties")
+match_playlist_pattern = re.compile(r"M\.(.*)\.properties")
 o0 = OsuPlaylist(oauth)
 font = "../unifont-15.1.05.otf"
 for i in os.listdir("./playlists"):
     if m := original_playlist_pattern.match(i):
-        shutil.copyfile("./playlists/%s" % m.group(0), "./playlists/%s" % m.group(1))
         o0.draw_target = True
         o0.draw_difficulty_table = False
-        o0.generate("./playlists/%s" % m.group(1), font, " — original playlist")
-        os.remove("./playlists/%s" % m.group(1))
-    if m := match_playlist_pattern.match(i):
-        shutil.copyfile("./playlists/%s" % m.group(0), "./playlists/%s" % m.group(1))
+        suffix = " — original playlist"
+    elif m := match_playlist_pattern.match(i):
+        suffix = " — match playlist"
         o0.draw_target = False
-        o0.draw_difficulty_table = True
-        o0.generate("./playlists/%s" % m.group(1), font, " — match playlist")
-        os.remove("./playlists/%s" % m.group(1))
+    else:
+        continue
+    o0.draw_difficulty_table = True
+    if os.path.exists("./playlists/%s.html" % m.group(1)) and fast_gen:
+        print("skipped %s" % m.group(1))
+        continue
+    shutil.copyfile("./playlists/%s" % m.group(0), "./playlists/%s.properties" % m.group(1))
+    o0.generate("./playlists/%s.properties" % m.group(1), font, suffix)
+    os.remove("./playlists/%s.properties" % m.group(1))
+    print("generated %s" % m.group(1))
