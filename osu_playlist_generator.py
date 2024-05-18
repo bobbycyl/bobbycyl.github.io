b2a765d3cdd3af4c72e16601e948fd67fc5e7728
bobbycyl <bobbycyl@126.com>
2024-05-18 13:34:47 +0800

    add HSC3S1R3

--- osu_playlist_generator.py
+++ osu_playlist_generator.py
@@ -9,6 +9,7 @@ fast_gen = True
 oauth = "../osu.properties"
 original_playlist_pattern = re.compile(r"O\.(.*)\.properties")
 match_playlist_pattern = re.compile(r"M\.(.*)\.properties")
+skill_practice_pattern = re.compile(r"SP\.(.*)\.properties")
 o0 = OsuPlaylist(oauth)
 font = "../unifont-15.1.05.otf"
 for i in os.listdir("./playlists"):
@@ -20,12 +21,21 @@ for i in os.listdir("./playlists"):
         suffix = " — match playlist"
         o0.draw_target = False
         o0.draw_difficulty_table = True
+    elif m := skill_practice_pattern.match(i):
+        suffix = " — original skill practice playlist"
+        o0.draw_target = False
+        o0.draw_difficulty_table = True
     else:
         continue
     if os.path.exists("./playlists/%s.html" % m.group(1)) and fast_gen:
         print("skipped %s" % m.group(1))
         continue
-    shutil.copyfile("./playlists/%s" % m.group(0), "./playlists/%s.properties" % m.group(1))
-    o0.generate("./playlists/%s.properties" % m.group(1), font, suffix)
-    os.remove("./playlists/%s.properties" % m.group(1))
-    print("generated %s" % m.group(1))
+    try:
+        shutil.copyfile("./playlists/%s" % m.group(0), "./playlists/%s.properties" % m.group(1))
+        o0.generate("./playlists/%s.properties" % m.group(1), font, suffix)
+    except:
+        raise
+    else:
+        print("generated %s" % m.group(1))
+    finally:
+        os.remove("./playlists/%s.properties" % m.group(1))
